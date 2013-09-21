ad5958141df6cc35298ac3e9bedb7e29b5a49806
josephktcheung <josephktcheung@gmail.com>
2013-09-21 22:54:30 +0800

    sending email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,7 @@ LearnRails::Application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -28,10 +28,10 @@ LearnRails::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.smtp_settings = {
-    :address    => "smtp.mandrillapp.com",
-    :port       => 25,
-    :user_name  => ENV["MANDRILL_USERNAME"],
-    :password   => ENV["MANDRILL_API_KEY"] 
+    address:              "smtp.mandrillapp.com",
+    port:                 587,
+    user_name:            ENV["MANDRILL_USERNAME"],
+    password:             ENV["MANDRILL_API_KEY"] 
   }
 
   # Send email in development mode.
